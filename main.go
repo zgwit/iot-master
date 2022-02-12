f6e214ddbbe3a3d7dfbf0015bbb1c6e2e7a73ee8
jason <jason@zgwit.com>
2022-02-12 16:49:53 +0800

    提交

--- main.go
+++ main.go
@@ -2,13 +2,19 @@ package main
 
 import (
 	"github.com/zgwit/iot-master/config"
+	"github.com/zgwit/iot-master/database"
 	"github.com/zgwit/iot-master/internal"
+	"github.com/zgwit/iot-master/tsdb"
 	"github.com/zgwit/iot-master/web"
 )
 
 func main() {
 
 	_ = internal.Start()
+
+	_ = database.Open(&config.Config.Database)
+
+	_ = tsdb.Open(&config.Config.History)
 
 	web.Serve(&config.Config.Web)
 }
