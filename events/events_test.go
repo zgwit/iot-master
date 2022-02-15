4fbbf7fae73952e3fa0de94ad93c7e725c4f7967
Jason <jason@zgwit.com>
2022-02-15 12:17:54 +0800

    添加一次测试

---
+++ events/events_test.go
@@ -0,0 +1,12 @@
+package events
+
+import "testing"
+
+func TestOnce(t *testing.T) {
+	e := EventEmitter{}
+	e.Once("test", func(arg interface{}) {
+		t.Log(arg)
+	})
+	e.Emit("test", "publish1")
+	e.Emit("test", "publish2")
+}
