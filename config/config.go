0e98f49ff4682d7f4a2c6fdae2d8c5df66ffaf1b
Jason <jason@zgwit.com>
2022-03-14 09:58:45 +0800

    配置文件解析

--- config/config.go
+++ config/config.go
@@ -1,9 +1,13 @@
 package config
 
 import (
+	"github.com/zgwit/iot-master/args"
 	"github.com/zgwit/iot-master/database"
 	"github.com/zgwit/iot-master/tsdb"
 	"github.com/zgwit/iot-master/web"
+	"gopkg.in/yaml.v3"
+	"log"
+	"os"
 )
 
 //Configure 配置
@@ -18,6 +22,41 @@ var Config Configure
 
 //Load 加载
 func Load() error {
+	//log.Println("加载配置")
+	//从参数中读取配置文件名
+	filename := args.ConfigPath
 
+	// 如果没有文件，则使用默认信息创建
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		return Store()
+	} else {
+		y, err := os.Open(filename)
+		if err != nil {
+			log.Fatal(err)
+			return err
+		}
+		defer y.Close()
+
+		d := yaml.NewDecoder(y)
+		return d.Decode(&Config)
+	}
 	return nil
+}
+
+func Store() error {
+	//log.Println("保存配置")
+	//从参数中读取配置文件名
+	filename := args.ConfigPath
+
+	y, err := os.OpenFile(filename, os.O_RDWR|os.O_CREATE, 0755) //os.Create(filename)
+	if err != nil {
+		log.Fatal(err)
+		return err
+	}
+	defer y.Close()
+
+	e := yaml.NewEncoder(y)
+	defer e.Close()
+
+	return e.Encode(&Config)
 }
