6f5634ebe0fede1e7f0ead2a869156f246fc35aa
Jason <jason@zgwit.com>
2022-04-05 21:26:34 +0800

    代码优化

--- events/events.go
+++ events/events.go
@@ -16,31 +16,31 @@ type EventInterface interface {
 }
 
 type EventEmitter struct {
-	handlers sync.Map
+	events sync.Map
 }
 
-type eventHandler struct {
+type subscriber struct {
 	callback reflect.Value
 	once     bool
 }
 
 //Emit 发送消息
 func (e *EventEmitter) Emit(event string, data ...interface{}) {
-	val, ok := e.handlers.Load(event)
+	val, ok := e.events.Load(event)
 	if !ok {
 		return
 	}
-	handlers := val.(*sync.Map)
+	subscribers := val.(*sync.Map)
 	args := make([]reflect.Value, 0)
 	for _, v := range data {
 		args = append(args, reflect.ValueOf(v))
 	}
-	handlers.Range(func(key, value interface{}) bool {
-		handler := value.(*eventHandler)
+	subscribers.Range(func(key, value interface{}) bool {
+		handler := value.(*subscriber)
 		handler.callback.Call(args)
 		//处理仅订阅一次
 		if handler.once {
-			handlers.Delete(key)
+			subscribers.Delete(key)
 		}
 		return true
 	})
@@ -49,13 +49,13 @@ func (e *EventEmitter) Emit(event string, data ...interface{}) {
 //On 监听
 func (e *EventEmitter) On(event string, fn interface{}) {
 	callback := reflect.ValueOf(fn)
-	val, ok := e.handlers.Load(event)
+	val, ok := e.events.Load(event)
 	if !ok {
 		val = new(sync.Map)
-		e.handlers.Store(event, val)
+		e.events.Store(event, val)
 	}
-	handlers := val.(*sync.Map)
-	handlers.Store(callback.Pointer(), &eventHandler{
+	subscribers := val.(*sync.Map)
+	subscribers.Store(callback.Pointer(), &subscriber{
 		callback: callback,
 		once:     false,
 	})
@@ -64,13 +64,13 @@ func (e *EventEmitter) On(event string, fn interface{}) {
 //Once 监听一次
 func (e *EventEmitter) Once(event string, fn interface{}) {
 	callback := reflect.ValueOf(fn)
-	val, ok := e.handlers.Load(event)
+	val, ok := e.events.Load(event)
 	if !ok {
 		val = new(sync.Map)
-		e.handlers.Store(event, val)
+		e.events.Store(event, val)
 	}
-	handlers := val.(*sync.Map)
-	handlers.Store(callback.Pointer(), &eventHandler{
+	subscribers := val.(*sync.Map)
+	subscribers.Store(callback.Pointer(), &subscriber{
 		callback: callback,
 		once:     true,
 	})
@@ -79,10 +79,10 @@ func (e *EventEmitter) Once(event string, fn interface{}) {
 //Off 取消监听
 func (e *EventEmitter) Off(event string, fn interface{}) {
 	callback := reflect.ValueOf(fn)
-	val, ok := e.handlers.Load(event)
+	val, ok := e.events.Load(event)
 	if !ok {
 		return
 	}
-	handlers := val.(*sync.Map)
-	handlers.Delete(callback.Pointer())
+	subscribers := val.(*sync.Map)
+	subscribers.Delete(callback.Pointer())
 }
