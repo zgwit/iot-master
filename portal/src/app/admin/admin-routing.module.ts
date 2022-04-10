a730af7dc6eeec32117f8ce769b237f68c0835b5
Jason <jason@zgwit.com>
2022-04-10 10:20:30 +0800

    添加一些路由

--- portal/src/app/admin/admin-routing.module.ts
+++ portal/src/app/admin/admin-routing.module.ts
@@ -17,6 +17,14 @@ import {ProtocolComponent} from "./protocol/protocol.component";
 import {SettingComponent} from "./setting/setting.component";
 import {UserComponent} from "./user/user.component";
 import {PasswordComponent} from "./password/password.component";
+import {TunnelDetailComponent} from "./tunnel-detail/tunnel-detail.component";
+import {LinkDetailComponent} from "./link-detail/link-detail.component";
+import {DeviceDetailComponent} from "./device-detail/device-detail.component";
+import {DeviceEditComponent} from "./device-edit/device-edit.component";
+import {ElementEditComponent} from "./element-edit/element-edit.component";
+import {ProjectDetailComponent} from "./project-detail/project-detail.component";
+import {ProjectEditComponent} from "./project-edit/project-edit.component";
+import {TemplateEditComponent} from "./template-edit/template-edit.component";
 
 const routes: Routes = [
   {
@@ -27,11 +35,25 @@ const routes: Routes = [
       {path: 'dash', component: DashComponent},
       {path: 'home', component: HomeComponent},
       {path: 'tunnel', component: TunnelComponent},
+      {path: 'tunnel/:id', component: TunnelDetailComponent},
+      //{path: 'tunnel/edit/:id', component: TunnelEditComponent},
+      //{path: 'tunnel/create', component: TunnelEditComponent},
       {path: 'link', component: LinkComponent},
+      {path: 'link/:id', component: LinkDetailComponent},
       {path: 'device', component: DeviceComponent},
+      {path: 'device/detail/:id', component: DeviceDetailComponent},
+      {path: 'device/edit/:id', component: DeviceEditComponent},
+      {path: 'device/create', component: DeviceEditComponent},
       {path: 'element', component: ElementComponent},
+      {path: 'element/edit/:id', component: ElementEditComponent},
+      {path: 'element/create', component: ElementEditComponent},
       {path: 'project', component: ProjectComponent},
+      {path: 'project/detail/:id', component: ProjectDetailComponent},
+      {path: 'project/edit/:id', component: ProjectEditComponent},
+      {path: 'project/create', component: ProjectEditComponent},
       {path: 'template', component: TemplateComponent},
+      {path: 'template/edit/:id', component: TemplateEditComponent},
+      {path: 'template/create', component: TemplateEditComponent},
       {path: 'plugin', component: PluginComponent},
       {path: 'protocol', component: ProtocolComponent},
       {path: 'setting', component: SettingComponent},
