e3607ddccb5f00ec37644b3f92103ec7954fc761
Jason <jason@zgwit.com>
2022-04-15 14:49:02 +0800

    删除From

--- model/aggregator.go
+++ model/aggregator.go
@@ -5,6 +5,5 @@ type Aggregator struct {
 	Targets    []string `json:"targets"`
 	Type       string   `json:"type"`
 	As         string   `json:"as"`
-	From       string   `json:"from"`
 	Expression string   `json:"expression"`
 }
