744df9a1587e6dcece5cd5341d3ad0bcb028311f
Jason <jason@zgwit.com>
2022-05-23 14:47:01 +0800

    Viewer OK了

---
+++ portal/src/app/hmi/lib.ts
@@ -0,0 +1,14 @@
+export function SvgToDataUrl(svg: string) {
+  return "data:image/svg+xml;base64," + atob(svg)
+}
+
+export function GetFieldDeeply(obj: any, key: string) {
+  let keys = key.split('.')
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i]
+    if (!obj.has(key))
+      return undefined
+    obj = obj[key]
+  }
+  return obj
+}
