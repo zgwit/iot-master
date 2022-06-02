04b094fd80a76ce7093126969a3ec91586bac7c7
Jason <jason@zgwit.com>
2022-06-02 22:54:11 +0800

    -w -s 会去除 DWARF调试信息、符号信息，节省一半空间

--- build.sh
+++ build.sh
@@ -17,6 +17,7 @@ fi
 gitHash=$(git show -s --format=%H)
 buildTime=$(date -d today +"%Y-%m-%d %H:%M:%S")
 
+# -w -s
 ldflags="-X 'github.com/zgwit/iot-master/args.Version=$version' \
 -X 'github.com/zgwit/iot-master/args.gitHash=$gitHash' \
 -X 'github.com/zgwit/iot-master/args.buildTime=$buildTime'"
