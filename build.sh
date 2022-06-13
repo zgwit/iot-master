0798b2856d1c3800f51f80cae5922ea741d4195e
Jason <jason@zgwit.com>
2022-06-13 10:09:06 +0800

    更新说明文档

--- build.sh
+++ build.sh
@@ -26,31 +26,36 @@ export GOARCH=amd64
 export GOOS=windows
 #CC=x86_64-w64-mingw32-gcc
 name="iot-master-windows-amd64.exe"
-go build -ldflags "$ldflags" -o "$name" main.go
-tar -zvcf $name.tar.gz $name & rm $name
+go build -ldflags "$ldflags" -o iot-master-windows-amd64.exe main.go
+tar -zvcf iot-master-windows-amd64.tar.gz iot-master-windows-amd64.exe
+rm iot-master-windows-amd64.exe
 
 export GOOS=linux
 #CC=gcc
 name="iot-master-linux-amd64"
-go build -ldflags "$ldflags" -o "$name" main.go
-tar -zvcf $name.tar.gz $name & rm $name
+go build -ldflags "$ldflags" -o iot-master-linux-amd64 main.go
+tar -zvcf iot-master-linux-amd64.tar.gz iot-master-linux-amd64
+rm iot-master-linux-amd64
 
 #export CC=arm-linux-gnueabihf-gcc
 
 export GOARCH=arm64
 name="iot-master-linux-arm64"
-go build -ldflags "$ldflags" -o "$name" main.go
-tar -zvcf $name.tar.gz $name & rm $name
+go build -ldflags "$ldflags" -o iot-master-linux-arm64 main.go
+tar -zvcf iot-master-linux-arm64.tar.gz iot-master-linux-arm64
+rm iot-master-linux-arm64
 
 export GOARCH=arm
 
 export GOARM=6
 name="iot-master-linux-armv6l"
-go build -ldflags "$ldflags" -o "$name" main.go
-tar -zvcf $name.tar.gz $name & rm $name
+go build -ldflags "$ldflags" -o iot-master-linux-armv6l main.go
+tar -zvcf iot-master-linux-armv6l.tar.gz iot-master-linux-armv6l
+rm iot-master-linux-armv6l
 
 export GOARM=7
 name="iot-master-linux-armv7l"
-go build -ldflags "$ldflags" -o "$name" main.go
-tar -zvcf $name.tar.gz $name & rm $name
+go build -ldflags "$ldflags" -o iot-master-linux-armv7l main.go
+tar -zvcf iot-master-linux-armv7l.tar.gz iot-master-linux-armv7l
+rm iot-master-linux-armv7l
 
