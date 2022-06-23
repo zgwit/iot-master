26cc8c2448c368595013fbf3c0edf3f1d6e0996a
Jason <jason@zgwit.com>
2022-06-23 17:46:12 +0800

    卡片化，编辑数据点 功能

--- portal/src/app/admin/edit-points/edit-points.component.ts
+++ portal/src/app/admin/edit-points/edit-points.component.ts
@@ -22,82 +22,53 @@ export class EditPointsComponent implements OnInit, ControlValueAccessor {
   }
 
   items: any[] = [];
+
   formGroup = new FormGroup({});
-  formArray: FormArray = new FormArray([]);
+
+  current: any = {};
+  showModal = false;
 
   constructor(private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
-    this.buildForm();
+    this.buildForm({});
   }
 
-  buildForm(): void {
+  buildForm(d: any): void {
     this.formGroup = this.fb.group({
-      items: this.formArray = this.fb.array(this.items.map((d: any) => {
-        return this.fb.group({
-          name: [d.name, [Validators.required]],
-          label: [d.label, []],
-          code: [d.code, [Validators.required]],
-          address: [d.address, [Validators.required]],
-          type: [d.type, [Validators.required]],
-          le: [d.le, [Validators.required]],
-          precision: [d.precision, [Validators.required]],
-          store: [d.store, [Validators.required]],
-          unit: [d.unit, []],
-        })
-      }))
+      name: [d.name, [Validators.required]],
+      label: [d.label, []],
+      code: [d.code, [Validators.required]],
+      address: [d.address, [Validators.required]],
+      type: [d.type, [Validators.required]],
+      le: [d.le, [Validators.required]],
+      precision: [d.precision, [Validators.required]],
+      store: [d.store, [Validators.required]],
+      unit: [d.unit, []],
     })
   }
 
-  add() {
-    this.formArray.push(this.fb.group({
-      name: ['', [Validators.required]],
-      label: ['', []],
-      code: ['', [Validators.required]],
-      address: ['', [Validators.required]],
-      type: ['', [Validators.required]],
-      le: [false, [Validators.required]],
-      precision: [0, [Validators.required]],
-      store: [true, [Validators.required]],
-      unit: ["", []],
-    }))
-    //复制controls，让表格可以刷新
-    this.formArray.controls = [...this.formArray.controls];
-    this.change();
-  }
-
   copy(i: number) {
-    const group = this.formArray.controls[i];
-
-    this.formArray.controls.splice(i, 0, this.fb.group({
-      name: [group.get('name')?.value, [Validators.required]],
-      label: [group.get('label')?.value, []],
-      code: [group.get('code')?.value, []],
-      address: [group.get('address')?.value, [Validators.required]],
-      type: [group.get('type')?.value, [Validators.required]],
-      le: [group.get('le')?.value, [Validators.required]],
-      precision: [group.get('precision')?.value, [Validators.required]],
-      store: [group.get('store')?.value, [Validators.required]],
-      unit: [group.get('unit')?.value, [Validators.required]],
-    }))
+    let item = this.items[i]
+    item = JSON.parse(JSON.stringify(item))
+    this.items.splice(i+1, 0, item)
   }
 
   remove(i: number) {
-    this.formArray.removeAt(i)
+    this.items.splice(i, 1)
     this.change();
   }
 
-
   clear() {
-    this.formArray.clear();
+    this.items = [];
     this.change();
   }
 
   change() {
-    this.formArray.markAsDirty();
-    this.formArray.updateValueAndValidity();
-    this.onChanged(this.formArray.value);
+    //this.formGroup.markAsDirty();
+    //this.formGroup.updateValueAndValidity();
+    this.onChanged(this.items);
     this.onTouched();
   }
 
@@ -111,12 +82,37 @@ export class EditPointsComponent implements OnInit, ControlValueAccessor {
 
   writeValue(obj: any): void {
     this.items = obj;
-    this.buildForm();
+    //this.buildForm();
   }
 
   drop($event: any) {
-    const item = this.formArray.controls.splice($event.previousIndex, 1);
-    this.formArray.controls.splice($event.currentIndex, 0, ...item);
+    const item = this.items.splice($event.previousIndex, 1);
+    this.items.splice($event.currentIndex, 0, ...item);
     this.change();
+  }
+
+  edit(data?: any) {
+    if (!data) {
+      data = {
+        name: '',
+        label: '',
+        code: '',
+        address: '',
+        type: '',
+        le: false,
+        precision: 0,
+        store: true,
+        unit: "",
+      }
+      this.items.push(data)
+    }
+    this.current = data;
+    this.buildForm(data)
+    this.showModal = true;
+  }
+
+  onOk() {
+    this.showModal = false;
+    Object.assign(this.current, this.formGroup.value)
   }
 }
