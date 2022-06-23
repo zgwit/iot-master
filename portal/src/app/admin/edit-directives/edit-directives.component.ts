a6779f37374dfabc614d45772f901047a9a09692
Jason <jason@zgwit.com>
2022-06-23 20:33:03 +0800

    编辑指令

--- portal/src/app/admin/edit-directives/edit-directives.component.ts
+++ portal/src/app/admin/edit-directives/edit-directives.component.ts
@@ -20,66 +20,48 @@ export class EditDirectivesComponent implements OnInit, ControlValueAccessor {
   }
 
   items: any[] = [];
+
   formGroup = new FormGroup({});
-  formArray: FormArray = new FormArray([]);
+
+  current: any = {};
+  showModal = false;
 
   constructor(private fb: FormBuilder) {
   }
 
   ngOnInit(): void {
-    this.buildForm();
+    this.buildForm({});
   }
 
-  buildForm(): void {
+  buildForm(d:any): void {
     this.formGroup = this.fb.group({
-      items: this.formArray = this.fb.array(this.items.map((d: any) => {
-        return this.fb.group({
           point: [d.point, [Validators.required]],
           value: [d.value, [Validators.required]],
           delay: [d.delay, [Validators.required]],
           expression: [d.expression, [Validators.required]],
-        })
-      }))
     })
   }
 
-  add() {
-    this.formArray.push(this.fb.group({
-      point: ['', [Validators.required]],
-      value: [0, [Validators.required]],
-      delay: [0, [Validators.required]],
-      expression: ['', [Validators.required]],
-    }))
-    //复制controls，让表格可以刷新
-    this.formArray.controls = [...this.formArray.controls];
-    this.change();
-  }
-
   copy(i: number) {
-    const group = this.formArray.controls[i];
-
-    this.formArray.controls.splice(i, 0, this.fb.group({
-      point: [group.get('point')?.value, [Validators.required]],
-      value: [group.get('value')?.value, [Validators.required]],
-      delay: [group.get('delay')?.value, [Validators.required]],
-      expression: [group.get('expression')?.value, [Validators.required]],
-    }))
+    let item = this.items[i]
+    item = JSON.parse(JSON.stringify(item))
+    this.items.splice(i+1, 0, item)
   }
 
   remove(i: number) {
-    this.formArray.removeAt(i)
+    this.items.splice(i, 1)
     this.change();
   }
 
   clear() {
-    this.formArray.clear();
+    this.items = [];
     this.change();
   }
 
   change() {
-    this.formArray.markAsDirty();
-    this.formArray.updateValueAndValidity();
-    this.onChanged(this.formArray.value);
+    //this.formGroup.markAsDirty();
+    //this.formGroup.updateValueAndValidity();
+    this.onChanged(this.items);
     this.onTouched();
   }
 
@@ -93,12 +75,32 @@ export class EditDirectivesComponent implements OnInit, ControlValueAccessor {
 
   writeValue(obj: any): void {
     this.items = obj;
-    this.buildForm();
+    //this.buildForm();
   }
 
   drop($event: any) {
-    const item = this.formArray.controls.splice($event.previousIndex, 1);
-    this.formArray.controls.splice($event.currentIndex, 0, ...item);
+    const item = this.items.splice($event.previousIndex, 1);
+    this.items.splice($event.currentIndex, 0, ...item);
     this.change();
+  }
+
+  edit(data?: any) {
+    if (!data) {
+      data = {
+        point: '',
+        value: '',
+        delay: '',
+        expression: '',
+      }
+      this.items.push(data)
+    }
+    this.current = data;
+    this.buildForm(data)
+    this.showModal = true;
+  }
+
+  onOk() {
+    this.showModal = false;
+    Object.assign(this.current, this.formGroup.value)
   }
 }
