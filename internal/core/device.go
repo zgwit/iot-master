6fba908a948eee483152e505febff00725e347b1
jason <jason@zgwit.com>
2023-02-05 11:50:03 +0800

    mqtt 整理

--- internal/core/device.go
+++ internal/core/device.go
@@ -1,9 +1,5 @@
 package core
 
-import (
-	"encoding/json"
-)
-
 var Devices Map[Device]
 
 type Device struct {
@@ -11,26 +7,3 @@ type Device struct {
 	Values map[string]any
 	Status map[string]any
 }
-
-func NewDevice(id string) *Device {
-	return &Device{
-		Id: id,
-	}
-}
-
-func (d *Device) Assign(points map[string]any) error {
-	data, err := json.Marshal(points)
-	if err != nil {
-		return err
-	}
-	return Publish("/device/"+d.Id+"/command/assign", data)
-}
-
-func (d *Device) Refresh() error {
-	return Publish("/device/"+d.Id+"/command/refresh", []byte(""))
-}
-
-//func (d *Device) Status() error {
-//	broker.MQTT.Publish("/device/"+d.Id+"/command/status", 0, false, "")
-//	return nil
-//}
--- internal/core/mqtt.go
+++ internal/core/mqtt.go
@@ -1,19 +1,22 @@
 package core
 
 import (
+	"context"
 	"fmt"
+	paho "github.com/eclipse/paho.mqtt.golang"
 	"github.com/mochi-co/mqtt/v2"
 	"github.com/mochi-co/mqtt/v2/hooks/auth"
 	"github.com/mochi-co/mqtt/v2/listeners"
-
-	//mqtt "github.com/eclipse/paho.mqtt.golang"
 	"github.com/zgwit/iot-master/v3/internal/db"
 	"github.com/zgwit/iot-master/v3/model"
 	"github.com/zgwit/iot-master/v3/pkg/log"
+	"github.com/zgwit/iot-master/v3/pkg/vconn"
+	"net"
 	"xorm.io/xorm"
 )
 
 var mqttServer *mqtt.Server
+var mqttClient paho.Client
 
 func OpenMqttServer() error {
 
@@ -23,14 +26,22 @@ func OpenMqttServer() error {
 	//TODO 鉴权
 	_ = mqttServer.AddHook(new(auth.AllowHook), nil)
 
-	err := loadListeners()
+	err := mqttLoadListeners()
 	if err != nil {
 		return err
 	}
 
-	//TODO websocket
+	err = mqttCreatePluginListener()
+	if err != nil {
+		return err
+	}
 
 	err = mqttServer.Serve()
+	if err != nil {
+		return err
+	}
+
+	err = mqttCreateInternalClient()
 	if err != nil {
 		return err
 	}
@@ -38,7 +49,7 @@ func OpenMqttServer() error {
 	return nil
 }
 
-func loadListeners() error {
+func mqttLoadListeners() error {
 	//监听服务
 	//加载数据库中 entrypoint
 	var entries []model.Server
@@ -58,13 +69,39 @@ func loadListeners() error {
 		}
 	}
 
-	//插件用
+	return nil
+}
+
+func mqttCreatePluginListener() error {
 	l := listeners.NewUnixSock("plugin", "iot-master.sock")
-	err = mqttServer.AddListener(l)
+	return mqttServer.AddListener(l)
+}
+
+func mqttCreateInternalClient() error {
+	//client := mqttServer.NewClient(nil, "internal", "internal", true)
+	opts := paho.NewClientOptions()
+	opts.AddBroker(":1883")
+	opts.SetClientID("internal")
+	//TODO 这里不生效，为啥
+	opts.SetDialer(&net.Dialer{
+		Resolver: &net.Resolver{Dial: func(ctx context.Context, network, address string) (net.Conn, error) {
+			c1, c2 := vconn.New()
+			_ = mqttServer.EstablishConnection("internal", c1)
+			return c2, nil
+		}},
+	})
+
+	mqttClient = paho.NewClient(opts)
+	token := mqttClient.Connect()
+	token.Wait()
+	err := token.Error()
 	if err != nil {
 		return err
 	}
+	//fmt.Println(token.Error())
 
+	//订阅消息
+	//subscribeTopics(MQTT)
 	return nil
 }
 
