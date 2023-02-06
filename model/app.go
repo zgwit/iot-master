0891459d712a23e846d463c1a10eeeee3543a835
jason <jason@zgwit.com>
2023-02-06 14:28:21 +0800

    添加字段

--- model/app.go
+++ model/app.go
@@ -3,18 +3,20 @@ package model
 import "time"
 
 type App struct {
+	Id      int64  `json:"id"`
 	Name    string `json:"name"`
 	Icon    string `json:"icon,omitempty"`
 	Label   string `json:"label"`
 	Desc    string `json:"desc"`
 	Type    string `json:"type"` //tcp unix
 	Address string `json:"address"`
+	Auth    string `json:"auth"`             //鉴权机制，继承 无 自定义，inherit none custom
 	Hidden  bool   `json:"hidden,omitempty"` //隐藏，适用于服务
 }
 
 type AppHistory struct {
 	Id      int64     `json:"id"`
-	Name    string    `json:"name"`
+	AppId   int64     `json:"app_id"`
 	Event   string    `json:"event"`
 	Created time.Time `json:"created" xorm:"created"`
 }
