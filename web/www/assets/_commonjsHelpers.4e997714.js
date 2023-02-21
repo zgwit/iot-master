22b37652197c8a2bec9c913e6218a5ef1bd71796
jason <jason@zgwit.com>
2023-02-22 07:14:46 +0800

    清除前端文件，EmbedFS无修改时间，导致静态文件重复加载，改用zip或www目录方式

--- web/www/assets/_commonjsHelpers.4e997714.js
+++
@@ -1,1 +0,0 @@
-var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof window?window:"undefined"!=typeof global?global:"undefined"!=typeof self?self:{};export{e as c};
--- web/www/assets/plugin-vue_export-helper.21dcd24c.js
+++
@@ -1,1 +0,0 @@
-var o=(o,r)=>{const t=o.__vccOpts||o;for(const[c,s]of r)t[c]=s;return t};export{o as _};
