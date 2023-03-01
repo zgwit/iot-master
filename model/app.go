fa3251509e259aea7058c8d26c2b93fc65a5c692
jason <jason@zgwit.com>
2023-03-02 06:31:01 +0800

    修改App定义

--- model/app.go
+++ model/app.go
@@ -2,16 +2,22 @@ package model
 
 import "time"
 
+type AppEntry struct {
+	Path string `json:"path"`
+	Name string `json:"name"`
+	Icon string `json:"icon,omitempty"`
+}
+
 type App struct {
-	Id      string `json:"id" xorm:"pk"`
-	Name    string `json:"name"`
-	Icon    string `json:"icon,omitempty"`
-	Entry   string `json:"entry,omitempty"`
-	Desc    string `json:"desc"`
-	Type    string `json:"type"` //tcp unix
-	Address string `json:"address"`
-	Auth    string `json:"auth"`             //鉴权机制，继承 无 自定义，inherit none custom
-	Hidden  bool   `json:"hidden,omitempty"` //隐藏，适用于服务
+	Id      string     `json:"id" xorm:"pk"`
+	Name    string     `json:"name"`
+	Icon    string     `json:"icon,omitempty"`
+	Entries []AppEntry `json:"entries"`
+	Desc    string     `json:"desc,omitempty"`
+	Type    string     `json:"type"` //tcp unix
+	Address string     `json:"address"`
+	Auth    string     `json:"auth"`             //鉴权机制，继承 无 自定义，inherit none custom
+	Hidden  bool       `json:"hidden,omitempty"` //隐藏，适用于服务
 }
 
 type AppHistory struct {
