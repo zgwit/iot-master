0927a0f9ee3405410b43825bba013902486e5335
Jason <jason@zgwit.com>
2023-03-10 17:59:25 +0800

    参数补充

--- model/device.go
+++ model/device.go
@@ -5,16 +5,17 @@ import (
 )
 
 type Device struct {
-	Id        string    `json:"id" xorm:"pk"` //ClientID
-	ProductId string    `json:"product_id" xorm:"index"`
-	DeviceId  string    `json:"device_id" xorm:"index"` //父设备
-	Type      string    `json:"type"`                   //网关/设备/子设备 gateway device subset
-	Name      string    `json:"name"`
-	Desc      string    `json:"desc"`
-	Username  string    `json:"username"`
-	Password  string    `json:"password"`
-	Disabled  bool      `json:"disabled"`
-	Created   time.Time `json:"created" xorm:"created"`
+	Id         string             `json:"id" xorm:"pk"` //ClientID
+	ProductId  string             `json:"product_id" xorm:"index"`
+	DeviceId   string             `json:"device_id" xorm:"index"` //父设备
+	Type       string             `json:"type"`                   //网关/设备/子设备 gateway device subset
+	Name       string             `json:"name"`
+	Desc       string             `json:"desc"`
+	Username   string             `json:"username"`
+	Password   string             `json:"password"`
+	Parameters map[string]float64 `json:"parameters"` //模型参数，用于报警检查
+	Disabled   bool               `json:"disabled"`
+	Created    time.Time          `json:"created" xorm:"created"`
 }
 
 type DeviceHistory struct {
