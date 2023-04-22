f5eb3ad0c1e4102f475655937252750de18a32cf
Jason <jason@zgwit.com>
2023-04-22 15:42:08 +0800

    版本号从tag中获取

--- build.sh
+++ build.sh
@@ -1,24 +1,21 @@
 
+#编译前端
+npm run build
+
 # 整体编译
 go env -w GOPROXY=https://goproxy.cn,direct
 go env -w GOPRIVATE=*.gitlab.com,*.gitee.com
 go env -w GOSUMDB=off
 
 app="iot-master"
-version="3.0.0"
-
-read -t 5 -p "please input version(default:$version)" ver
-if [ -n "${ver}" ];then
-	version=$ver
-fi
-
 
 pkg="github.com/zgwit/iot-master/v3/pkg/build"
+gitTag=$(git tag -l | tail -n 1)
 gitHash=$(git show -s --format=%H)
 buildTime=$(date -d today +"%Y-%m-%d %H:%M:%S")
 
 # -w -s
-ldflags="-X '${pkg}.Version=$version' \
+ldflags="-X '${pkg}.Version=$gitTag' \
 -X '${pkg}.GitHash=$gitHash' \
 -X '${pkg}.Build=$buildTime'"
 
