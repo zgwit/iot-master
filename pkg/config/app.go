cff4771de75225981a81f8cb3db0720912013f45
jason <jason@zgwit.com>
2023-04-24 12:53:09 +0800

    bug

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -7,6 +7,10 @@ import (
 )
 
 func AppName() string {
+	//插件的args参数为空
+	if len(os.Args) == 0 {
+		return "iot-master"
+	}
 	app, _ := filepath.Abs(os.Args[0])
 	ext := filepath.Ext(os.Args[0])
 	return strings.TrimSuffix(app, ext)
