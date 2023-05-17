367ccc23e497468d74a71f0949835d41151aec6e
T-r-ee <1311337083@qq.com>
2023-05-17 16:05:47 +0800

    图片上传

--- src/app/product/product-edit-component/product-edit-component.component.ts
+++ src/app/product/product-edit-component/product-edit-component.component.ts
@@ -5,6 +5,7 @@ import { Router } from "@angular/router";
 import { RequestService } from "../../request.service";
 import { NzMessageService } from "ng-zorro-antd/message";
 import { isIncludeAdmin } from "../../../public";
+import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
 
 @Component({
   selector: 'app-product-edit-component',
@@ -164,6 +165,17 @@ export class ProductEditComponentComponent implements OnInit {
       }
     })
 
+  }
+
+  handleChange(info: NzUploadChangeParam): void {
+    if (info.file.status !== 'uploading') {
+      // console.log(info.file, info.fileList);
+    }
+    if (info.file.status === 'done') {
+      this.msg.success(`${info.file.name} 文件上传成功`);
+    } else if (info.file.status === 'error') {
+      this.msg.error(`${info.file.name} 文件上传失败.`);
+    }
   }
 
   handleCancel() {
