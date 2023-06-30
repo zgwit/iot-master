20c30e1cc83bbb9c595648d773a4f15225c5232a
Jason <jason@zgwit.com>
2023-06-30 10:21:43 +0800

    优化

--- model/alarm.go
+++ model/alarm.go
@@ -8,8 +8,8 @@ type Alarm struct {
 	Id        int64     `json:"id"`
 	ProductId string    `json:"product_id,omitempty" xorm:"index"`
 	DeviceId  string    `json:"device_id,omitempty" xorm:"index"`
-	Product   string    `json:"product,omitempty" xorm:"-"`
-	Device    string    `json:"device,omitempty" xorm:"-"`
+	Product   string    `json:"product,omitempty" xorm:"<-"`
+	Device    string    `json:"device,omitempty" xorm:"<-"`
 	Type      string    `json:"type,omitempty"`
 	Title     string    `json:"title"`
 	Message   string    `json:"message,omitempty"`
@@ -23,8 +23,8 @@ type Subscription struct {
 	UserId    string    `json:"user_id" xorm:"index"`
 	ProductId string    `json:"product_id,omitempty" xorm:"index"`
 	DeviceId  string    `json:"device_id,omitempty" xorm:"index"`
-	Product   string    `json:"product,omitempty" xorm:"-"`
-	Device    string    `json:"device,omitempty" xorm:"-"`
+	Product   string    `json:"product,omitempty" xorm:"<-"`
+	Device    string    `json:"device,omitempty" xorm:"<-"`
 	Level     uint      `json:"level"`
 	Channels  []string  `json:"channels" xorm:"json"`
 	Disabled  bool      `json:"disabled"` //禁用
--- model/history.go
+++ model/history.go
@@ -7,7 +7,7 @@ import (
 type History struct {
 	Id       int64     `json:"id"`
 	DeviceId string    `json:"device_id" xorm:"index"`
-	Device   string    `json:"device,omitempty" xorm:"-"`
+	Device   string    `json:"device,omitempty" xorm:"<-"`
 	Point    string    `json:"point" xorm:"index"` //数据点
 	Value    float64   `json:"value"`              //值
 	Time     time.Time `json:"time"`
