4945443441cede48373a38367defa35df82f7c29
Jason <jason@zgwit.com>
2024-04-11 01:16:20 +0800

    更新依赖

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	//_ "github.com/iot-master-contrib/camera"
-	//_ "github.com/iot-master-contrib/webui"
+	_ "github.com/iot-master-contrib/camera"
+	_ "github.com/iot-master-contrib/influxdb"
+	_ "github.com/iot-master-contrib/webui"
 	master "github.com/zgwit/iot-master/v4"
 	"github.com/zgwit/iot-master/v4/args"
 	"github.com/zgwit/iot-master/v4/config"
